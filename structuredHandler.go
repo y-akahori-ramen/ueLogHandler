7f74614869aac919b5f0db86639e8ea699f70070
Yuki Akahori <y.akahori.ramen@gmail.com>
2022-05-21 21:39:37 +0900

    Add missing commit

---
+++ structuredHandler.go
@@ -0,0 +1,81 @@
+package ueloghandler
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type StructuredLogHandler struct {
+	handlers map[string]StructuredLogDataHandler
+}
+
+func NewStructuredLogHandler() *StructuredLogHandler {
+	return &StructuredLogHandler{handlers: make(map[string]StructuredLogDataHandler)}
+}
+
+func (h *StructuredLogHandler) AddHandler(handler StructuredLogDataHandler) error {
+	if _, ok := h.handlers[handler.Type()]; ok {
+		return fmt.Errorf("handler already exists: %s", handler.Type())
+	} else {
+		h.handlers[handler.Type()] = handler
+		return nil
+	}
+}
+
+func (h *StructuredLogHandler) HandleLog(log Log) error {
+	type Header struct {
+		Type string
+	}
+	type Body map[string]interface{}
+
+	results, _ := GetStructuredDataFromLog[Header, Body](log.Log)
+
+	if len(results) != 0 {
+		for _, result := range results {
+			structureType := result.Header.Type
+			if _, ok := h.handlers[structureType]; !ok {
+				return fmt.Errorf("invalid structure type: %s", structureType)
+			} else {
+				jsonStr, err := json.Marshal(result.Body)
+				if err != nil {
+					return err
+				}
+
+				err = h.handlers[structureType].Handle(string(jsonStr), log)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	} else {
+		return nil
+	}
+}
+
+type StructuredLogDataHandler interface {
+	Handle(json string, log Log) error
+	Type() string
+}
+
+func NewStructuredLogDataHandler[THeader, TBody any](typeName string, handleFunc func(TStructuredData[THeader, TBody], Log) error) StructuredLogDataHandler {
+	return &tStructuredLogDataHandler[THeader, TBody]{typeName: typeName, handleFunc: handleFunc}
+}
+
+type tStructuredLogDataHandler[THeader, TBody any] struct {
+	handleFunc func(TStructuredData[THeader, TBody], Log) error
+	typeName   string
+}
+
+func (h *tStructuredLogDataHandler[THeader, TBody]) Type() string {
+	return h.typeName
+}
+
+func (h *tStructuredLogDataHandler[THeader, TBody]) Handle(json string, log Log) error {
+	data, err := JSONToStructuredData[THeader, TBody](json)
+	if err != nil {
+		return err
+	}
+
+	return h.handleFunc(data, log)
+}
---
+++ structuredHandler_test.go
@@ -0,0 +1,143 @@
+package ueloghandler_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	ueloghandler "github.com/y-akahori-ramen/ueLogHandler"
+)
+
+type handlerTestVector struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+type handlerTestHeader struct {
+	HeaderString string
+	HeaderInt    int
+	HeaderFloat  float64
+}
+
+type handlerTestBody struct {
+	BodyString string
+	BodyInt    int
+	BodyVector handlerTestVector
+}
+
+type handlerTestStructureData ueloghandler.TStructuredData[handlerTestHeader, handlerTestBody]
+
+type handlerTestCase struct {
+	structureTypeName string
+	jsonStrings       []string
+	wantErr           error
+	wantData          []handlerTestStructureData
+	actualData        []handlerTestStructureData
+}
+
+func (tc *handlerTestCase) Handle(data ueloghandler.TStructuredData[handlerTestHeader, handlerTestBody], log ueloghandler.Log) error {
+	tc.actualData = append(tc.actualData, handlerTestStructureData(data))
+	return nil
+}
+
+func TestStructuredLogHandler(t *testing.T) {
+	testCases := []handlerTestCase{
+		{
+			structureTypeName: "TestStructure",
+			jsonStrings: []string{
+				`{"Header":{"HeaderString":"test1","HeaderInt":1,"HeaderFloat":1.0},"Body":{"BodyString":"test1","BodyInt":2,"BodyVector":{"X":1.1,"Y":1.2,"Z":1.3}}}`,
+			},
+			wantErr: nil,
+			wantData: []handlerTestStructureData{
+				{
+					Header: handlerTestHeader{
+						HeaderString: "test1",
+						HeaderInt:    1,
+						HeaderFloat:  1.0,
+					},
+					Body: handlerTestBody{
+						BodyString: "test1",
+						BodyInt:    2,
+						BodyVector: handlerTestVector{
+							X: 1.1,
+							Y: 1.2,
+							Z: 1.3,
+						},
+					},
+				},
+			},
+		},
+		{
+			structureTypeName: "TestStructure",
+			jsonStrings: []string{
+				`{"Header":{"HeaderString":"testHeader1","HeaderInt":1,"HeaderFloat":1.0},"Body":{"BodyString":"testBody1","BodyInt":2,"BodyVector":{"X":1.1,"Y":1.2,"Z":1.3}}}`,
+				`{"Header":{"HeaderString":"testHeader2","HeaderInt":2,"HeaderFloat":2.0},"Body":{"BodyString":"testBody2","BodyInt":3,"BodyVector":{"X":2.1,"Y":2.2,"Z":2.3}}}`,
+			},
+			wantErr: nil,
+			wantData: []handlerTestStructureData{
+				{
+					Header: handlerTestHeader{
+						HeaderString: "testHeader1",
+						HeaderInt:    1,
+						HeaderFloat:  1.0,
+					},
+					Body: handlerTestBody{
+						BodyString: "testBody1",
+						BodyInt:    2,
+						BodyVector: handlerTestVector{
+							X: 1.1,
+							Y: 1.2,
+							Z: 1.3,
+						},
+					},
+				},
+				{
+					Header: handlerTestHeader{
+						HeaderString: "testHeader2",
+						HeaderInt:    2,
+						HeaderFloat:  2.0,
+					},
+					Body: handlerTestBody{
+						BodyString: "testBody2",
+						BodyInt:    3,
+						BodyVector: handlerTestVector{
+							X: 2.1,
+							Y: 2.2,
+							Z: 2.3,
+						},
+					},
+				},
+			},
+		},
+		{
+			structureTypeName: "InvalidType",
+			wantErr:           errors.New("invalid structure type: InvalidType"),
+			jsonStrings: []string{
+				`{"Header":{"HeaderString":"test1","HeaderInt":1,"HeaderFloat":1.0},"Body":{"BodyString":"test1","BodyInt":2,"BodyVector":{"X":1.1,"Y":1.2,"Z":1.3}}}`,
+			},
+		},
+	}
+
+	for i := range testCases {
+		testCase := testCases[i]
+		t.Run(fmt.Sprintf("Case%d", i), func(t *testing.T) {
+			assert := assert.New(t)
+
+			logStr := ""
+			for _, json := range testCase.jsonStrings {
+				logStr += ueloghandler.BeginStructuredStr + fmt.Sprintf(`{"Header":{"Type":"%s"},"Body":%s}`, testCase.structureTypeName, json) + ueloghandler.EndStructuredStr
+			}
+
+			logHandler := ueloghandler.NewStructuredLogHandler()
+
+			dataHandler := ueloghandler.NewStructuredLogDataHandler("TestStructure", testCase.Handle)
+			logHandler.AddHandler(dataHandler)
+
+			err := logHandler.HandleLog(ueloghandler.Log{Log: logStr})
+			assert.Equal(testCase.wantErr, err)
+			assert.Equal(testCase.wantData, testCase.actualData)
+		})
+	}
+}
